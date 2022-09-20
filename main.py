c672e27885016a43c1037c1e99a987cd3a78db51
CherryDP <90273335+CherryDP@users.noreply.github.com>
2022-09-20 17:04:43 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,30 +1,48 @@
-class Wozi:
+class dziaua:
     def __init__(self,tab):
         self.Tab = tab
 
-    def sort_word(self):
-        new_list = []
+    def mergeSort(self):
+        if len(array) > 1:
+            r = len(self.Tab)//2
+            L = self.Tab[:r]
+            M = self.Tab[r:]
 
-        while self.Tab:
-            min = self.Tab[0]
-            for x in self.Tab:
-                if x < min:
-                    min = x
-            new_list.append(min)
-            self.Tab.remove(min)
-        self.list = new_list
-        return self.list
 
-    def __duplicat__(self):
+            i = j = k = 0
+
+            while i < len(L) and j < len(M):
+                if L[i] < M[j]:
+                    self.Tab[k] = L[i]
+                    i += 1
+                else:
+                    self.Tab[k] = M[j]
+                    j += 1
+                k += 1
+
+            while i < len(L):
+                self.Tab[k] = L[i]
+                i += 1
+                k += 1
+
+            while j < len(M):
+                self.Tab[k] = M[j]
+                j += 1
+                k += 1
+
+    def printList(self):
         newlist = []
         duplist = []
-        for i in self.list:
+        for i in self.Tab:
             if i not in newlist:
                 newlist.append(i)
             else:
                 duplist.append(i)
         print("List of duplicates", duplist)
 
-d1 = Wozi(['A', 'Z', 'K', 'J', 'J', 'B', 'D', 'A', 'E', 'C'])
-d1.sort_word()
-d1.__duplicat__()
+
+
+array = ['A', 'Z', 'K', 'J', 'B', 'D', 'A', 'E', 'C']
+d1 = dziaua(array)
+d1.mergeSort()
+d1.printList()
